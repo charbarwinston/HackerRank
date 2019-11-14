19d07f752fa39d89280c5b7d54ee116d5ba009d8
Neff Charles A <NeffCharlesA@JohnDeere.com>
2019-11-14 15:13:21 -0600

    why didn't this add'

---
+++ ProblemSolving/Python/MagicSquare.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the formingMagicSquare function below.
+def formingMagicSquare(s):
+    print(s)
+    
+
+if __name__ == '__main__':
+
+    s = [[5,3,4],[1,5,8],[6,4,2]]
+
+    print(formingMagicSquare(s))
+
+
