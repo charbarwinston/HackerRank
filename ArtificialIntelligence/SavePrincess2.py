1fc71d5c0f5a57d94a53b34bbaf81c4d4b873814
Neff Charles A <NeffCharlesA@JohnDeere.com>
2019-11-14 15:12:47 -0600

    solved the mario bot

--- ArtificialIntelligence/SavePrincess2.py
+++ ArtificialIntelligence/SavePrincess2.py
@@ -3,30 +3,27 @@
 def displayPathtoPrincess(n,grid):
     m = []
     p = []
-    print(enumerate(grid))
     for y, row in enumerate(grid):
-        print(y, row)
         if 'm' in row:
             m = [row.index('m'), y]
         if 'p' in row:
             p = [row.index('p'), y] 
    
-    print(m,p)
     while m[0] != p[0]:
         if m[0] < p[0]:
-            print('RIGHT')
+            return('RIGHT')
             m[0] += 1
         elif m[0] > p[0]:
-            print('LEFT')
+            return('LEFT')
             m[0] -= 1
         
 
     while m[1]!= p[1]:
         if m[1] < p[1]:
-            print('DOWN')
+            return('DOWN')
             m[1] += 1
         elif m[1] > p[1]:
-            print('UP')
+            return('UP')
             m[1] -= 1
         
 
