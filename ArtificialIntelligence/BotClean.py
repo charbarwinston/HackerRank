b5f695625656cc62f8da1abfe98ca39920fa00f5
Neff Charles A <NeffCharlesA@JohnDeere.com>
2019-11-27 11:16:47 -0600

    Created Gui for bot clean lol

--- ArtificialIntelligence/BotClean.py
+++ ArtificialIntelligence/BotClean.py
@@ -3,22 +3,31 @@
 from math import sqrt
 
 import tkinter as tk
+import time
 
 def updateLoop(frame):
     def move():
-        nextMove = next_move()
-        if nextMove == "CLEAN":
-            cleanTile()
-        elif nextMove == "RIGHT":
-            moveRight()
-        elif nextMove == "LEFT":
-            moveLeft()
-        elif nextMove == "DOWN":
-            moveUp()
-        elif nextMove == "UP":
-            moveDown()
+        if not checkWin():
+            nextMove = next_move()
+            if nextMove == "CLEAN":
+                cleanTile()
+            elif nextMove == "RIGHT":
+                moveRight()
+            elif nextMove == "LEFT":
+                moveLeft()
+            elif nextMove == "DOWN":
+                moveDown()
+            elif nextMove == "UP":
+                moveUp()
+            updateBoard()
+            frame.after(1000, move)
+        else:
+            winAnimation()
+    def winAnimation():
+        tileToGreen(frame)
         updateBoard()
-        frame.after(1000, move)
+        frame.after(100, winAnimation)
+        
     move()
 
 def next_move():
@@ -27,6 +36,7 @@ def next_move():
     posr = pos[1]
     global board
     if board[posc][posr] == 'd':
+        print("clean")
         return("CLEAN")
     closestPoint = []
     closestDistance = 999999999
@@ -40,50 +50,54 @@ def next_move():
                     closestDistance = distance
                     closestPoint = [rowIndex,y]
     if closestPoint[0] > posr:
+        print('right')
         return('RIGHT')
     elif closestPoint[0] < posr:
+        print('left')
         return('LEFT')
     elif closestPoint[1] > posc:
+        print('down')
         return('DOWN')
     elif closestPoint[1] < posc:
+        print('up')
         return('UP')
 
 def cleanTile():
     global pos
     global board
-    board[pos[0]][pos[1]].replace('d','b')
+    board[pos[0]][pos[1]] = 'b'
 
 def moveRight():
     global pos
     global board
     board[pos[0]][pos[1]] = '-'
-    if board[pos[0] + 1][pos[1]] != 'd':
-        board[pos[0] + 1][pos[1]] = 'b'
-    pos = [pos[0] + 1, pos[1]]
-
-def moveLeft():
-    global pos
-    global board
-    board[pos[0]][pos[1]] = "-"
-    if board[pos[0] - 1][pos[1]] != 'd':
-        board[pos[0] - 1][pos[1]] = 'b'
-    pos = [pos[0] - 1, pos[1]]
-
-def moveDown():
-    global pos
-    global board
-    board[pos[0]][pos[1]] = "-"
     if board[pos[0]][pos[1] + 1] != 'd':
         board[pos[0]][pos[1] + 1] = 'b'
     pos = [pos[0], pos[1] + 1]
 
-def moveUp():
+def moveLeft():
     global pos
     global board
     board[pos[0]][pos[1]] = "-"
     if board[pos[0]][pos[1] - 1] != 'd':
         board[pos[0]][pos[1] - 1] = 'b'
     pos = [pos[0], pos[1] - 1]
+
+def moveDown():
+    global pos
+    global board
+    board[pos[0]][pos[1]] = "-"
+    if board[pos[0] + 1][pos[1]] != 'd':
+        board[pos[0] + 1][pos[1]] = 'b'
+    pos = [pos[0] + 1, pos[1]]
+
+def moveUp():
+    global pos
+    global board
+    board[pos[0]][pos[1]] = "-"
+    if board[pos[0] - 1][pos[1]] != 'd':
+        board[pos[0] - 1][pos[1]] = 'b'
+    pos = [pos[0] - 1, pos[1]]
 
 def initializeBoard(board, frame):
     labels = {}
@@ -92,13 +106,13 @@ def initializeBoard(board, frame):
             if i not in labels:
                 labels[i] = []
             if char == 'b':
-                labels[i].append(tk.Label(frame, bg="blue", width=10, height=5))
+                labels[i].append(tk.Label(frame, bg="blue", width=6, height=3))
                 labels[i][ci].grid(row=i,column=ci)
             elif char == 'd':
-                labels[i].append(tk.Label(frame, bg="brown", width=10, height=5))
+                labels[i].append(tk.Label(frame, bg="brown", width=6, height=3))
                 labels[i][ci].grid(row=i,column=ci)
             elif char == '-':
-                labels[i].append(tk.Label(frame, bg="gray", width=10, height=5))
+                labels[i].append(tk.Label(frame, bg="gray", width=6, height=3))
                 labels[i][ci].grid(row=i,column=ci)
     return(labels)
 
@@ -114,14 +128,33 @@ def updateBoard():
                 labels[i][ci].configure(bg="brown")
             elif char == '-':
                 labels[i][ci].configure(bg="gray")
+            elif char == 'g':
+                labels[i][ci].configure(bg="green")
 
-pos = [0,1]
+def checkWin():
+    global board
+    for charList in board:
+        for char in charList:
+            if char == 'd':
+                return False
+    return True
+
+def tileToGreen(frame):
+    global board
+    for index, charList in enumerate(board):
+        for charIndex, char in enumerate(charList):
+            if char != 'g':
+                board[index][charIndex] = 'g'
+                break
+
+
+pos = [0,0]
 board = [
-        "bd---",
-        "-d---",
-        "---d-",
-        "--d--",
-        "----d"
+        list("bd---"),
+        list("-d---"),
+        list("---d-"),
+        list("--d--"),
+        list("----d")
         ]
 #next_move(pos[0], pos[1], board)
 
